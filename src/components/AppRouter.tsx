81db7765a44d857e581456b7d086e4499689489e
AntonFartovii <anton@rza.by>
2023-03-22 19:56:58 +0300

    feat: merge task 1

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,45 +1,46 @@
-import React, {Component} from 'react';
-import {Routes, Route, Navigate} from 'react-router-dom'
-import {IRoute, routesAll} from "../routes";
+import React, { Component } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { IRoute } from '../routes';
 import { ABOUT_US, FORM_ROUTE, MAIN_ROUTE, PAGE_404 } from '../constants/pages';
 import MainPage from '../pages/MainPage';
 import AboutPage from '../pages/AboutPage';
+import Page404 from '../pages/Page404';
 import FormPage from '../pages/FormPage';
 
 interface IRouterProps {
-  showPageName?: (name: string) => void
+  showPageName?: (name: string) => void;
 }
 
-interface IRouterState {
+export default class AppRouter extends Component<IRouterProps> {
+  constructor(props: IRouterProps) {
+    super(props);
+  }
 
+  renderRoute(route: IRoute) {
+    const Component: React.ReactElement = route.Component;
+    // Component.props.showPageName = this.props.showPageName
+    return <Route key={route.path} path={route.path} element={Component} />;
+  }
+
+  render() {
+    return (
+      <Routes>
+        <Route
+          path={MAIN_ROUTE}
+          element={<MainPage showPageName={this.props.showPageName} />}
+        ></Route>
+        <Route
+          path={FORM_ROUTE}
+          element={<FormPage showPageName={this.props.showPageName} />}
+        ></Route>
+        <Route
+          path={ABOUT_US}
+          element={<AboutPage showPageName={this.props.showPageName} />}
+        ></Route>
+        <Route path="/404" element={<Page404 showPageName={this.props.showPageName} />}></Route>
+
+        <Route path="*" element={<Navigate to={PAGE_404}/>}></Route>
+      </Routes>
+    );
+  }
 }
-
-export default class AppRouter extends Component<IRouterProps, IRouterState> {
-
-    constructor(props: IRouterProps) {
-      super(props);
-    }
-
-    renderRoute(route: IRoute) {
-      const Component: React.ReactElement = route.Component
-      // Component.props.showPageName = this.props.showPageName
-      return <Route key={route.path} path={route.path} element={Component}/>
-    }
-
-    render() {
-        return (
-                <Routes>
-                    {/*{*/}
-                    {/*    routesAll.map( this.renderRoute )*/}
-                    {/*}*/}
-                    {/*<Route path="*" element={<Navigate to={PAGE_404}/>} />*/}
-                  <Route path={MAIN_ROUTE} element={<MainPage showPageName={this.props.showPageName}/>}/>
-                  <Route path={FORM_ROUTE} element={<FormPage showPageName={this.props.showPageName}/>}/>
-                  <Route path={ABOUT_US} element={<AboutPage showPageName={this.props.showPageName}/>}/>
-                  <Route path="*" element={<Navigate to={PAGE_404}/>} />
-                </Routes>
-
-        );
-    }
-};
-
--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,27 +1,24 @@
-import React from "react";
+import React from 'react';
 import { IPageProps } from './MainPage';
 
-export default class AboutPage extends React.Component<IPageProps, any> {
-    name: string
+export default class AboutPage extends React.Component<IPageProps> {
+    name: string;
 
     constructor(props: IPageProps) {
         super(props);
-        this.name = 'О нас'
-        this.showTest = this.showTest.bind( this )
+        this.name = 'О нас';
+        this.showTest = this.showTest.bind(this);
     }
 
     showTest() {
-        this.props.showPageName &&
-        this.props.showPageName( this.name )
+        this.props.showPageName && this.props.showPageName(this.name);
     }
 
     componentDidMount(): void {
-        this.showTest()
+        this.showTest();
     }
 
     render() {
-        return(
-            <h1>About us</h1>
-        )
+        return <h1>About us</h1>;
     }
 }
