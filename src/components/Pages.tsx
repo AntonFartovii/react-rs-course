554cd6644279e896a2fc3c79a40c3c818dcf8825
AntonFartovii <anton@rza.by>
2023-03-30 18:49:04 +0300

    feat: rewrite Pages on hooks

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -1,38 +1,23 @@
-import React, { ReactNode } from 'react';
+import React, { useState } from 'react';
 import Header from '../common/Header';
 import Content from '../common/Content';
 import AppRouter from './AppRouter';
-interface IPagesProps {
-  children?: ReactNode;
-}
 
-interface IPagesState {
-  namePage: string;
-}
+const Pages = () => {
+  const [namePage, setNamePage]: [string, any] = useState('')
 
-export default class Pages extends React.Component<IPagesProps, IPagesState> {
-  constructor(props: IPagesProps) {
-    super(props);
-    this.state = {
-      namePage: '',
-    };
-    this.handlePageNameChange = this.handlePageNameChange.bind(this);
+  const handlePageNameChange = (name: string) => {
+    setNamePage( name );
   }
 
-  handlePageNameChange(name: string) {
-    this.setState({
-      namePage: name,
-    });
-  }
-
-  render() {
-    return (
+  return (
       <>
-        <Header currentPage={this.state.namePage} />
+        <Header currentPage={namePage} />
         <Content>
-          <AppRouter showPageName={this.handlePageNameChange} />
+          <AppRouter showPageName={handlePageNameChange} />
         </Content>
       </>
     );
-  }
 }
+
+export default Pages
