f58bae196df3a3634dad8eb56d614d3c24bf8605
AntonFartovii <anton@rza.by>
2023-04-01 22:10:38 +0300

    feat: inputs text,date,file,checkbox validation

--- src/components/CustomInputElement.tsx
+++ src/components/CustomInputElement.tsx
@@ -1,48 +1,90 @@
 import React, { ForwardedRef, forwardRef } from 'react';
 import { FieldValues, UseFormRegister } from 'react-hook-form';
+import { logDOM } from '@testing-library/dom';
 
 export interface ICustomTextInputProps {
   onChange?: () => void;
-  label: string;
+  name: string;
   error?: string;
   type: string;
-  ref: any
-  // ref: UseFormRegister<FieldValues>;
+  refProp: UseFormRegister<FieldValues>;
+  values?: string[]
 }
 
 interface IInputElements {
   [elem: string]: JSX.Element;
 }
 
-const CustomInputElement = ({onChange, label, error, type, ref}: ICustomTextInputProps) => {
-    label = label.toLowerCase();
+const CustomInputElement = (
+    {name, error, type, refProp, values}: ICustomTextInputProps) => {
+    name = name.toLowerCase();
 
     const elements: IInputElements = {
-      text: <input id={label} type="text" {...ref}
-                   data-testid={label} />,
-      radio: <input id={label} type="radio" {...ref} />,
-      number: <input id={label} type="number" {...ref} />,
-      textarea: <input id={label} type="text" {...ref} />,
-      date: <input id={label} type="date" {...ref} />,
-      file: (
-        <>
-          <input id={label} type="file" {...ref} />
-          <img src="" alt="" className="new-img" />
-        </>
-      ),
+      text: <input
+                id={name}
+                type="text"
+                data-testid={name}
+                {...refProp(name, {
+                  required: 'Field is empty',
+                  minLength: 0,
+                  validate: value => {return ''}
+                })}
+      />,
+      radio: <input
+                id={name}
+                type="radio"
+                {...refProp(name, {
+                  required: '',
+                  validate: value => {return ''}
+                })} />,
+      number: <input
+                id={name}
+                type="number"
+                {...refProp(name, {
+                  required: 'Field is empty',
+                  minLength: 0,
+                  validate: value => {return ''}
+                })} />,
+      date: <input
+                id={name}
+                type="date"
+                {...refProp(name, {
+                  required: 'Date in not chosen',
+                  validate: value => {
+                    const date = new Date().toDateString()
+                    return Date.parse(value) < Date.parse( date ) || `Must be < ${date}`
+                  }})} />,
+      file: <input
+                id={name}
+                type="file"
+                {...refProp(name, {
+                  required: 'There is not file',
+                  validate: (value) => {
+                    if (!/^image/.test(value[0].type)) {
+                      return 'This file is not image type';
+                    }
+                  }
+                })} />,
       checkbox: (
-        <>
-          I agree to the terms and conditions
-          <input id={label} type="checkbox" {...ref} />
-        </>
+                <>
+                  I agree to the terms and conditions
+                  <input
+                    id={name}
+                    type="checkbox"
+                    value="agree"
+                    {...refProp(name,{
+                      required: 'You must to agree',
+                      validate: value => {return ''}
+                    })} />
+                </>
       ),
     };
 
     return (
       <div className="form-field">
-        <label htmlFor={label}>{label.toUpperCase()}</label>
+        <label htmlFor={name}>{name.toUpperCase()}</label>
         {elements[type] && elements[type]}
-        <div className="validation" data-testid={`val_${label}`}>
+        <div className="validation" data-testid={`val_${name}`}>
           {error && error}
         </div>
       </div>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,73 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import CustomInputElement from './CustomInputElement';
+import { STATE_GOOD } from '../constants/pages';
 
+interface IForm {
+  title?: string;
+  description?: string;
+  date?: string;
+  file?: string | FileList;
+}
 
 const FormCard = () => {
-  const {register, handleSubmit} = useForm()
+  const {register, handleSubmit, reset, formState: { errors }} = useForm({
+    mode: 'onSubmit', reValidateMode: 'onSubmit' })
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: IForm) => {
     console.log(data);
+    reset()
   }
 
     return (
-      <form
-        className="card-form"
-        onSubmit={handleSubmit(onSubmit)}
-        data-testid="form"
-      >
-        <input type="text" {...register('title')}/>
+      <form className="card-form" onSubmit={handleSubmit(onSubmit)} data-testid="form">
+        <CustomInputElement name="title"       type="text" refProp={register} error={errors?.title?.message?.toString()}/>
+        <CustomInputElement name="description" type="text" refProp={register} error={errors?.description?.message?.toString()}/>
+        <CustomInputElement name="date"        type="date" refProp={register} error={errors?.date?.message?.toString()}/>
+        <CustomInputElement name="file"        type="file" refProp={register} error={errors?.file?.message?.toString()}/>
+        <div className="form-field">
+          <div className="row">
+            <div className="column-left">
+              <CustomInputElement name="price" type="number" refProp={register} error={errors?.price?.message?.toString()}/>
+            </div>
+            <div className="column-right">
+              {/*<CustomInputElement name="currency" type="select" refProp={register} error={errors?.currency?.message?.toString()}/>*/}
+            </div>
+          </div>
+        </div>
+        <div className="form-field">
+          <div className="row">
+            <div className="column-left">
+              <input
+                id="radio1"
+                type="radio"
+                value={STATE_GOOD.OLD}
+                {
+                  ...register('state', {
+                    required: 'Choose please one option'
+                  })
+                }
+              />
+              <label htmlFor="radio1">б/у</label>
+            </div>
+            <div className="column-right">
+              <input
+                id="radio2"
+                type="radio"
+                value={STATE_GOOD.NEW}
+                {
+                  ...register('state', {
+                    required: 'Choose please one option'
+                  })
+                }
+              />
+              <label htmlFor="radio1">новый</label>
+            </div>
+          </div>
+          <div className="validation">{errors.state?.message?.toString()}</div>
+        </div>
 
+        <CustomInputElement name="condition" type="checkbox" refProp={register} error={errors?.condition?.message?.toString()}/>
         <div className="form-field">
           <button type="submit">Send</button>
         </div>
